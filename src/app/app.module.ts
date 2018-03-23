46abe2a6420a24e48bc86bb522f15566b48ccf30
Nelson Dornelas <ndornelasj@gmail.com>
2018-03-23 15:34:06 -0700

    feat: dropdown toggle open with custom dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    DropdownDirective
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleDropdown(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
+}
